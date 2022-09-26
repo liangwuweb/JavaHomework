59bc6da36ea20877f2c34abae35a9d1198ea77e0
Liang Wu <liangwuweb@gmail.com>
2022-09-26 09:35:53 -0500

    remove unused import

--- src/com/liangwu/java/DeckDriver.java
+++ src/com/liangwu/java/DeckDriver.java
@@ -1,7 +1,5 @@
 package com.liangwu.java;
 
-import java.util.Arrays;
-
 public class DeckDriver {
     static String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     static String[] suits = {"C", "S", "H", "D"};
